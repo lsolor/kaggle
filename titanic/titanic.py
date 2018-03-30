e78f9b7219a64acbe0af7c6d7c6aed05053565ae
wildyeo58 <lsolorza@oberlin.edu>
2018-03-30 10:20:24 -0400

    adding more description to README

---
+++ titanic/titanic.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sys
+import random
+
+
+
+def main():
+    train = "train.csv"
+    test = "test.csv"
+    seed = 1234 #temp value for testing
+
+    
+    
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
